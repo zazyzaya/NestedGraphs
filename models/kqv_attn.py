3494c2047d65af112b8f352cda484d37b4645f86
zazyzaya <zazyzaya@gmail.com>
2022-02-11 16:06:08 -0500

    Finally got sparse transformer working

--- models/kqv_attn.py
+++ models/kqv_attn.py
@@ -3,7 +3,7 @@ import torch
 from torch import nn 
 from torch.nn.utils.rnn import pad_packed_sequence
 
-from .utils import repack, packed_cat
+from .utils import repack, packed_cat, masked_softmax, get_last_vectors_unpadded
 
 class KQV_Attention(nn.Module):
     '''
@@ -35,6 +35,7 @@ class KQV_Attention(nn.Module):
         )
 
     def forward(self,ts,seq):
+        # Combine time series and features into single packed sequence
         x = packed_cat([ts, seq])
 
         # Don't want to pad yet, as it adds a bunch of uncecessary 
@@ -43,10 +44,26 @@ class KQV_Attention(nn.Module):
         k = repack(self.key(x.data), x)
         q = repack(self.query(x.data), x)
 
-        # We've avoided this as long as we could
-        k,batch_sizes = pad_packed_sequence(k, batch_first=True)
-        q,_ = pad_packed_sequence(q, batch_first=True)
-
-        attn = torch.softmax(q @ k.T, dim=1)
+        # But its eventually inevitable
+        k,_ = pad_packed_sequence(k, batch_first=True)
         v,_ = pad_packed_sequence(v, batch_first=True)
+
+        # As we only care about the final value in the sequence,
+        # rather than doing a whole mat mul, we can just take the
+        # dot product of each q and V matrix. 
+        #
+        # Note that the final matrix is (QK^T)V and we only want
+        # the last row of it, which is 
+        # [(q_n * k_1) * [v_11, v_21, ...] , (q_n * k_2) * [v_12, v_22, ...], etc]
+        # Only dependant on the last non-zero row of Q, so why waste the time 
+        q = get_last_vectors_unpadded(q)
+        attn = q.unsqueeze(1) @ k.transpose(1,2)
+
+        # Removes any values derived from columns further in the future 
+        # than exist for this datapoint. Note that zeros only occur when 
+        # a padded value influences the weight of an element. We dont even
+        # need to know batch sizes, since irrelevant columns are multiplied
+        # by the guaranteed zero in masked_softmax
+        attn = masked_softmax(attn, 2)
+        return (attn @ v).squeeze(1)
         
--- models/utils.py
+++ models/utils.py
@@ -36,3 +36,54 @@ def packed_cat(seq, dim=1):
         sorted_indices=x.sorted_indices, 
         unsorted_indices=x.unsorted_indices
     )
+
+def get_last_vectors_padded(seq, batch_sizes):
+    '''
+    Selects the vector at each time step in seq at 
+    sequence position batch_size. Useful for grabbing the
+    last vector in a padded sequence
+
+    Inputs: 
+        [T, B, *] tensor (seq len, batch size, dim)
+        [B] tensor of indexes to get
+
+    Output: 
+        [B, *] tensor of vectors s.t. seq[i] returns batch_sizes[i]
+    '''
+    # I made this a lot harder than it had to be initially.. but it's
+    # still annoying to type this every time. This is more concise
+    return seq[torch.arange(seq.size(0)), batch_sizes-1]
+
+
+def get_last_vectors_unpadded(seq):
+    '''
+    Returns the last value for each element in a PackedSequence 
+    object (not padded!)
+    '''
+    idxs = []
+    i = -1
+    num_found = 0
+    end = seq.data.size(0)
+    
+    while len(idxs) < seq.sorted_indices.size(0):
+        start = end-seq.batch_sizes[i]
+        idx_start = start + num_found
+        found = seq.sorted_indices[num_found:seq.batch_sizes[i]]
+        
+        if found.size():
+            idxs += list(range(idx_start, end))
+            num_found += len(found)
+        
+        end = start
+        i -= 1
+    
+    mapped_indices = torch.zeros(seq.sorted_indices.size(0), dtype=torch.int64)
+    mapped_indices[seq.sorted_indices] = torch.tensor(idxs)
+    return seq.data[mapped_indices]
+    
+def masked_softmax(A, dim):
+    # matrix A is the one you want to do mask softmax at dim=1
+    A_max = torch.max(A,dim=dim,keepdim=True)[0]
+    A_exp = torch.exp(A-A_max)
+    A_exp = A_exp * (A != 0).type(torch.FloatTensor) # this step masks
+    return A_exp / (torch.sum(A_exp,dim=dim,keepdim=True)+10**-6)
