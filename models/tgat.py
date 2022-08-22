6ba8231eca3551d69cb75185a763aa36e74352b9
zazyzaya <zazyzaya@gmail.com>
2022-08-22 13:36:18 -0400

    Finished TGAT impl

--- models/tgat.py
+++ models/tgat.py
@@ -1,5 +1,9 @@
+import math 
+
 import torch 
-from torch import nn 
+from torch import nn
+
+from models.embedder import KQV 
 
 class TimeKernel(nn.Module):
     '''
@@ -11,15 +15,18 @@ class TimeKernel(nn.Module):
         assert dim % 2 == 0, 'TimeKernel must have an even output dimension'
         
         self.w = nn.Linear(1,dim//2)
-        self.norm = torch.sqrt(1 / (dim//2))
+        self.norm = math.sqrt(1 / (dim//2))
+        self.dim = dim
 
     def forward(self, t):
         t = t.max()-t
         t = self.w(t)
-        return torch.cat([
+
+        # Get sin/cos to be interleaved
+        return torch.stack([
             torch.sin(t),
             torch.cos(t)
-        ], dim=1).true_divide(self.norm)
+        ], dim=2).view(t.size(0),self.dim) * self.norm
 
 class QKV(nn.Module):
     def __init__(self, in_feats, hidden):
@@ -31,28 +38,40 @@ class QKV(nn.Module):
         return self.q(z), self.k(z), self.v(z)
 
 class NeighborhoodAggr(nn.Module):
-    def __init__(self, in_feats, t_feats, hidden, out_feats):
+    def __init__(self, hidden, heads, TKernel):
         super().__init__()
+        assert hidden % heads == 0, 'KQV feat dim must be divisible by heads'
 
         # Note that W( x||t2v(t) ) = W_1(x) + W_2(t2v)
         # so we can calculate K,Q,V on their own, then add in 
         # the temporal part as if it were part of the initial parameter
-        self.t2v = TimeKernel(t_feats)
-        self.time_param = nn.Linear(t_feats, in_feats)
-        self.proj = nn.Sequential(
-            nn.Linear(in_feats+hidden, out_feats),
-            nn.ReLU()
-        )
+        self.t2v = TKernel
+        self.time_params = QKV(TKernel.dim, hidden)
 
-    def forward(self, x_0, k,q,v,t, graph):
+        self.hidden = hidden 
+        self.heads = heads
+        self.head_dim = hidden // heads
+        self.norm = math.sqrt(1/self.head_dim)
+
+    def forward(self, nid,k,q,v,t, graph):
+        '''
+        Takes as input: 
+            nid: the index of the target node 
+            k,q,v: the key, query, value matrices for all nodes
+            t: the current time 
+            graph: a graph datastructure specified by preprocessing.datastructures.HostGraph
+        '''
         # 1xd
-        q = q[x_0].unsqueeze(0)
+        q = q[nid].unsqueeze(0)
 
-        neighbors,times = graph.one_hop[x_0]
+        neighbors,times = graph.one_hop[nid]
         t_mask = times <= t 
 
+        if t_mask.sum() == 0:
+            return torch.zeros(1,self.hidden)
+
         k,v = k[neighbors[t_mask]], v[neighbors[t_mask]]
-        t = self.time_param(
+        t_q, t_k, t_v = self.time_params(
             self.t2v(
                 torch.cat(
                     [torch.tensor([[t]]), times], 
@@ -62,15 +81,66 @@ class NeighborhoodAggr(nn.Module):
         )
 
         # Factor time into the original matrices
-        q += t[0]; k += t[1:]; v += t[1:]
+        q += t_q[0]; k += t_k[1:]; v += t_v[1:]
+
+        # Use reshape optimization for multihead attn 
+        # heads x 1 x d/heads
+        q = q.view(1, self.heads, self.head_dim).transpose(0,1)
+        
+        # heads x d/heads x batch
+        k = k.view(k.size(0), self.heads, self.head_dim)
+        k = k.transpose(-1,1).transpose(-1,0)
+
+        # heads x batch x d/heads
+        v = v.view(v.size(0),self.heads,self.head_dim).transpose(0,1)
 
         # Perform self-attention calculation
-        attn = torch.softmax(q@k.T, dim=1)
-        h = attn @ v 
+        # output is heads x 1 x d/heads
+        # then, "catted" together as 1 x d 
+        attn = torch.softmax((q @ k) * self.norm, dim=1)        
+        return (attn @ v).view(1,self.hidden)
 
-        return self.proj(torch.cat([q, ]))
 
+class TGAT_Layer(nn.Module):
+    def __init__(self, in_feats, hidden, out_feats, heads, TKernel):
+        super().__init__()
+
+        self.kqv = KQV(in_feats, hidden)
+        self.neighbor_aggr = NeighborhoodAggr(hidden, heads, TKernel)
+        
+        # Paper uses 2-layer FFNN w/ no final activation and intermediate ReLU
+        self.lin = nn.Sequential(
+            nn.Linear(hidden+in_feats, hidden),
+            nn.ReLU(),
+            nn.Linear(hidden, out_feats)
+        )
+
+    
+    def forward(self, graph, x, t):
+        h = []
+        k,q,v = self.kqv(x)
+
+        # TODO node-level parallelism in this for-loop
+        for nid in range(graph.num_nodes):
+            h.append(self.neighbor_aggr(nid, k,q,v,t,graph))
+
+        h = torch.cat(h,dim=0)
+        x = torch.cat([x,h], dim=1)
+        return self.lin(x)
 
 class TGAT(nn.Module):
-    def __init__(self, in_feats, hidden, t2v):
-        super().__init__()
+    def __init__(self, in_feats, t_feats, hidden, out, layers, heads):
+        super().__init__() 
+        tkernel = TimeKernel(t_feats)
+
+        self.layers = nn.ModuleList(
+            [TGAT_Layer(in_feats, hidden, hidden, heads, tkernel)] + 
+            ([TGAT_Layer(hidden, hidden, hidden, heads, tkernel)] * layers-2) + 
+            [TGAT_Layer(hidden, hidden, out, heads, tkernel)]
+        )
+
+    def forward(self, graph, x, t):
+        for layer in self.layers: 
+            x = layer(graph, x, t)
+
+        return x 
