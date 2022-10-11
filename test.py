0ec1d8ad2eaaca4f55b0e1b2c69e6d85e4e04abc
zazyzaya <zazyzaya@gmail.com>
2022-10-11 15:39:06 -0400

    The bug was I commened out the save command >>

--- test.py
+++ test.py
@@ -1,3 +1,18 @@
-from preprocessing.build_hostgraph import build_full_graph
+import pickle
+import glob 
 
-build_full_graph(1,1,503,23,True,False)
+HOME = 'inputs/Sept23/mal/'
+files = glob.glob(HOME+'*')
+
+def check_file(fname):
+    with open(fname, 'rb') as f:
+        g = pickle.load(f)
+
+    for i in range(g.x.size(0)):
+        g.get(i)
+
+for f in files:
+    check_file(f)
+
+#from preprocessing.build_hostgraph import build_full_graph
+#build_full_graph(0,1,503,23,True,False)
